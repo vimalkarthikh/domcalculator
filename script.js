5d775f28c331f0cb265ca496386f7b122ad1c086
vimalkarthikh <137371585+vimalkarthikh@users.noreply.github.com>
2023-07-24 20:53:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+var t=document.createElement("h1");t.setAttribute("id","title");t.innerHTML="Calculator";document.body.append(t);
 var ip=document.createElement("input");
-ip.setAttribute("type","text");ip.setAttribute("id","screen");document.body.append(ip);
+ip.setAttribute("type","text");ip.setAttribute("id","result");document.body.append(ip);
 
 function bt(cval,idval){
     var b=document.createElement("button");
@@ -10,16 +11,16 @@ function bt(cval,idval){
 function bl(){var b=document.createElement("br");return b;}
 var n=bl();var n1=bl();var n2=bl();var n3=bl();var n4=bl();
 
-var clr=bt("clear()","C");var del=bt("del()","DEL");var div=bt("dsp('/')","/");
+var clr=bt("clear()","C");var dl=bt("del()","DEL");var div=bt("dsp('/')","/");
 var nn=bt("dsp('9')","9");var eg=bt("dsp('8')","8");var sn=bt("dsp('7')","7");
 var sx=bt("dsp('6')","6");var fv=bt("dsp('5')","5");var fr=bt("dsp('4')","4");
 var th=bt("dsp('3')","3");var tw=bt("dsp('2')","2");var on=bt("dsp('1')","1");
-var zr=bt("dsp('0')","0");var equ=bt("calcu('=')","=");
-var add=bt("dsp('+')","+");var sub=bt("dsp('-')","-");var mul=bt("dsp('*')","*");
+var zr=bt("dsp('0')","0");var dzr=bt("dsp('00')","00");var equ=bt("calcu('=')","=");var dt=bt("dsp('.')",".");
+var add=bt("dsp('+')","+");var sub=bt("dsp('-')","-");var mul=bt("dsp('*')","x");
 
-document.body.append(n,nn,eg,sn,add,n1,sx,fv,fr,sub,n2,th,tw,on,div,n3,clr,zr,mul,equ);
+document.body.append(n,nn,eg,sn,add,n1,sx,fv,fr,sub,n2,th,tw,on,div,n3,dzr,zr,mul,equ,n4,clr,dl,dt);
 
-let os=document.getElementById("screen");
+let os=document.getElementById("result");
 
 function dsp(num){
     os.value+=num;
@@ -28,8 +29,8 @@ function calcu(){
     try{os.value=eval(os.value);}
     catch(err){alert("Invalid Input");}
 }
-function clr(){
-    os.value="";
+function clear(){
+    os.value=" ";
 }
 function del(){
     os.value=os.value.slice(0,-1);
